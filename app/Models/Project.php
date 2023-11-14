295e6d036d9f7119bd5dae26b609589deac5e205
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-14 14:53:35 +0100

    creata tabella pivo
    creata migration e modificati i models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -16,5 +17,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
